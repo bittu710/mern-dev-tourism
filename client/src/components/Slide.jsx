05882ab0f30abb4cecbe8c8e011434487a545e03
bittu710 <omkardebnathabc@gmail.com>
2024-08-02 23:44:33 +0530

    Initial commit

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! Anywhere you roam <br /> Stay in the moment. Make your wonderful
+        memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
